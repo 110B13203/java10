d320c0e4d3a48fe4ddc8801ae89efd00ea93e2a2
110B13203 <112997321+110B13203@users.noreply.github.com>
2022-10-30 03:34:59 +0800

    Add files via upload

---
+++ Class17.java
@@ -0,0 +1,33 @@
+package java10;
+class Car{
+	protected String owner;
+	protected String id;
+	public Car(String own,String s) {
+		owner=own;
+		id=s;
+	}
+	public void show() {
+		System.out.println("車主姓名:"+owner);
+		System.out.println("車牌號碼:"+id);
+	}
+}
+class CColor extends Car{
+	private String color;
+	public CColor(String own,String s,String c) {
+		super(own, s);
+		color=c;
+	}
+	public void show() {
+		System.out.println("車主姓名:"+owner);
+		System.out.println("車牌號碼:"+id);
+		System.out.println("車身顏色:"+color);
+	}
+}
+public class Class17 {
+
+	public static void main(String[] args) {
+		CColor mycar=new CColor("Riaan","A1-2345","Black");
+		mycar.show();
+	}
+
+}
